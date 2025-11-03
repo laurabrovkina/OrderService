00a37ccdc6c4c49fbbd1f58b6589e92b11373285
larisa <laurabrovkina@gmail.com>
2025-11-04 12:24:23 +1300

    Setup GetOrderByUsernameHandler and Request

---
+++ OrderService/Application/Queries/GetOrderByUsernameHandler.cs
@@ -0,0 +1,12 @@
+﻿using Mediator;
+using OrderService.GraphQL.Types;
+
+namespace OrderService.Application.Queries;
+
+public class GetOrderByUsernameHandler : IRequestHandler<GetOrderByUsernameRequest, Order>
+{
+    public ValueTask<Order> Handle(GetOrderByUsernameRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ OrderService/Application/Queries/GetOrderByUsernameRequest.cs
@@ -0,0 +1,9 @@
+﻿using Mediator;
+using OrderService.GraphQL.Types;
+
+namespace OrderService.Application.Queries;
+
+public class GetOrderByUsernameRequest : IRequest<Order>
+{
+    public string? Username { get; set; }
+}
