7522deadee6589ca8c91661a70dcb01c2a054a47
larisa <laurabrovkina@gmail.com>
2025-11-04 12:25:06 +1300

    Register services

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HotChocolate.Execution;
+using OrderService.Data.DatabaseConfiguration;
+using OrderService.GraphQL;
+using OrderService.GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMediator();
+
+builder.Services.AddDbContextPool<OrderDbReadContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("OrderDb")));
+
+await builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddType<Order>()
+    .UseDefaultPipeline()
+    .BuildSchemaAsync();
+    
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<OrderDbReadContext>();
+    dbContext.Database.EnsureCreated();
+}
+
+app.MapGraphQL();
+
+app.RunWithGraphQLCommands(args);
