7972f1e681dd06065f5b7de005f6f13c06bd9741
larisa <laurabrovkina@gmail.com>
2025-11-06 14:34:50 +1300

    Add BasicBuilder

---
+++ OrderService.IntegrationTests/Builder/BaseBuilder.cs
@@ -0,0 +1,32 @@
+﻿namespace OrderService.IntegrationTests.Builder;
+
+public abstract class BaseBuilder<T> where T : class, new()
+{
+    protected T Model { get; }
+    
+    protected BaseBuilder()
+    {
+        Model = new T();
+    }
+
+    private Queue<Action<T>> _propertySettingActions = new();
+
+    public BaseBuilder<T> With(Action<T> setPropertyAction)
+    {
+        _propertySettingActions.Enqueue(setPropertyAction);
+
+        return this;
+    }
+
+    public virtual T Build()
+    {
+        foreach (var action in _propertySettingActions)
+        {
+            action(Model);
+        }
+
+        _propertySettingActions = new Queue<Action<T>>();
+        
+        return Model;
+    }
+}
---
+++ OrderService.IntegrationTests/Builder/OrderBuilder.cs
@@ -0,0 +1,21 @@
+﻿using OrderService.Data.DatabaseConfiguration;
+
+namespace OrderService.IntegrationTests.Builder;
+
+public class OrderBuilder : BaseBuilder<OrderDbOrder>
+{
+    public OrderBuilder BasicOrderBuilder()
+    {
+        Model.Username = "TestUser";
+        Model.OrderTypes = [
+            new OrderDbOrderType
+            {
+                OrderId = Guid.NewGuid(),
+                Name = "Test Order Type",
+                Order = Model
+            }
+        ];
+
+        return this;
+    }
+}
