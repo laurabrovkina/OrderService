587b021053ac59e26ea47dd33e74b2c88f19a1be
larisa <laurabrovkina@gmail.com>
2025-11-07 15:24:36 +1300

    First working test!

--- OrderService.IntegrationTests/Tests/Queries/GetOrderByUsername.cs
+++ OrderService.IntegrationTests/Tests/Queries/GetOrderByUsername.cs
@@ -1,4 +1,5 @@
-﻿using OrderService.IntegrationTests.Builder;
+﻿using OrderService.Data.DatabaseConfiguration;
+using OrderService.IntegrationTests.Builder;
 using Shouldly;
 using Xunit;
 
@@ -18,15 +19,22 @@ public class GetOrderByUsername : IClassFixture<TestFixture>
     {
         // Arrange
         var client = _fixture.GetOrderServiceClient();
+
         var order = new OrderBuilder().BasicOrderBuilder()
-            .With(c => c.Username = "100011")
+            .With(c => c.Username = Guid.NewGuid().ToString())
             .Build();
-        
-        await _fixture.CreateOrder(order);
+        var orderType = new OrderDbOrderType
+        {
+            Name = "Individual",
+            Orders = [order]
+        };
+
+        await _fixture.CreateOrder(order, orderType);
         
         // Act
         var response = await client.GetOrderByUserName.ExecuteAsync(order.Username);
         await _fixture.DeleteOrder(order);
+        await _fixture.DeleteOrderType(orderType);
         
         // Assert
         response.Data.ShouldNotBeNull();
