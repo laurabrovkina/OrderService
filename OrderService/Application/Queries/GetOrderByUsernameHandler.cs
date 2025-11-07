807131d15c9af5fce206fd93c4523b251a74ceb1
larisa <laurabrovkina@gmail.com>
2025-11-07 15:15:24 +1300

    Implement handler logic

--- OrderService/Application/Queries/GetOrderByUsernameHandler.cs
+++ OrderService/Application/Queries/GetOrderByUsernameHandler.cs
@@ -1,12 +1,35 @@
 ﻿using Mediator;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Application.Mappings;
+using OrderService.Data.DatabaseConfiguration;
 using OrderService.GraphQL.Types;
 
 namespace OrderService.Application.Queries;
 
+/// <summary>
+/// Handles the request to retrieve an order by a specified username.
+/// </summary>
+/// <remarks>
+/// This handler processes a query to fetch a user's order by their username.
+/// It interacts with the database context to retrieve the relevant order data.
+/// </remarks>
 public class GetOrderByUsernameHandler : IRequestHandler<GetOrderByUsernameRequest, Order>
 {
-    public ValueTask<Order> Handle(GetOrderByUsernameRequest request, CancellationToken cancellationToken)
+    private readonly OrderDbReadContext _orderDbContext;
+
+    public GetOrderByUsernameHandler(OrderDbReadContext orderDbContext)
     {
-        throw new NotImplementedException();
+        _orderDbContext = orderDbContext;
+    }
+
+    public async ValueTask<Order> Handle(GetOrderByUsernameRequest request, CancellationToken cancellationToken)
+    {
+        var order = await _orderDbContext.Orders
+            .Where(x => x.Username == request.Username)
+            .FirstOrDefaultAsync(cancellationToken: cancellationToken);
+
+        _ = order ?? throw new Exception("Orders not found");
+
+        return order.ToOrder();
     }
 }
